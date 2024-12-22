0ed1a5a603024ef1e089f0998ec7be5351e14a3a
pix <pixqc1159@gmail.com>
2024-12-22 07:48:55 +0000

    wip better parsers

--- lua/mana.lua
+++ lua/mana.lua
@@ -9,7 +9,7 @@ local M = {}
 ---@alias Mana2.ModelConfig table<Mana2.Model, Mana2.ModelConfig_>
 ---@alias Mana2.BufferState {winid: integer|nil, bufnr: integer}
 
--- // WINDOW+BUFFER stuffs --
+-- // WINDOW+BUFFER STUFFS --
 
 ---move cursor down to "textbox"
 ---@param bufnr integer
@@ -92,21 +92,7 @@ local function window_create(bufnr)
 	return winid
 end
 
----@param cfgs table<Mana2.Endpoint, {url: string, env: string}>
----@return Mana2.EndpointConfig
-local function mk_endpoint_cfgs(cfgs)
-	local cfgs_ = {}
-	for endpoint, config in pairs(cfgs) do
-		local api_key = os.getenv(config.env)
-		if api_key then
-			cfgs_[endpoint] = {
-				url = config.url,
-				api_key = api_key,
-			}
-		end
-	end
-	return cfgs_
-end
+-- // CHAT STUFFS --
 
 ---@param data string
 ---@return string
@@ -171,6 +157,62 @@ local function fetch(model_cfg, endpoint_cfg, bufnr)
 end
 
 ---@param bufnr integer
+---@param fetcher function
+---@return nil
+local function keymap_set_chat(bufnr, fetcher)
+	vim.api.nvim_buf_set_keymap(bufnr, "n", "<CR>", "", {
+		callback = function()
+			local messages = buffer_parse(bufnr)
+			if #messages == 1 and messages[1].content == "" then
+				print("emtpy input")
+				return -- no user input, do nothing
+			end
+			if messages then
+				buffer_append(bufnr, "\n\n<assistant>\n\n")
+				fetcher(messages)
+			end
+		end,
+		noremap = true,
+		silent = true,
+	})
+end
+
+---@param bufnr integer
+---@param model_cfgs Mana2.ModelConfig
+---@param fetchers function[]
+---@return nil
+local function command_set_chat(bufnr, model_cfgs, fetchers)
+	vim.api.nvim_create_user_command("Mana", function(opts)
+		local args = vim.split(opts.args, "%s+")
+		local cmd = args[1]
+		if cmd == "switch" then
+			local model = args[2]
+			if not model then
+				vim.notify("Please specify a model name", vim.log.levels.ERROR)
+				return
+			end
+			local cfg = model_cfgs[model]
+			if not cfg then
+				vim.notify("Invalid model name: " .. model, vim.log.levels.ERROR)
+				return
+			end
+			keymap_set_chat(fetchers[model], bufnr)
+			vim.api.nvim_buf_set_lines(bufnr, 0, 1, false, {
+				string.format("model: %s", cfg.name),
+			})
+		end
+	end, {
+		nargs = 1,
+		range = true,
+		complete = function()
+			return { "switch" }
+		end,
+	})
+end
+
+-- // UI STUFFS --
+
+---@param bufnr integer
 ---@return nil
 local function keymap_set_ui(bufnr)
 	-- clear chat
@@ -186,27 +228,6 @@ local function keymap_set_ui(bufnr)
 	vim.api.nvim_buf_set_keymap(bufnr, "i", "<C-n>", "", {
 		callback = function()
 			buffer_clear(bufnr)
-		end,
-		noremap = true,
-		silent = true,
-	})
-end
-
----@param bufnr integer
----@param fetcher function
----@return nil
-local function keymap_set_chat(bufnr, fetcher)
-	vim.api.nvim_buf_set_keymap(bufnr, "n", "<CR>", "", {
-		callback = function()
-			local messages = buffer_parse(bufnr)
-			if #messages == 1 and messages[1].content == "" then
-				print("emtpy input")
-				return -- no user input, do nothing
-			end
-			if messages then
-				buffer_append(bufnr, "\n\n<assistant>\n\n")
-				fetcher(messages)
-			end
 		end,
 		noremap = true,
 		silent = true,
@@ -259,38 +280,7 @@ local function command_set_ui(bufnr, winid)
 	})
 end
 
--- ---@param bufnr integer
--- ---@param model_cfgs Mana2.ModelConfig
--- ---@param fetchers function[]
--- ---@return nil
--- local function command_set_chat(bufnr, model_cfgs, fetchers)
--- 	vim.api.nvim_create_user_command("Mana", function(opts)
--- 		local args = vim.split(opts.args, "%s+")
--- 		local cmd = args[1]
--- 		if cmd == "switch" then
--- 			local model = args[2]
--- 			if not model then
--- 				vim.notify("Please specify a model name", vim.log.levels.ERROR)
--- 				return
--- 			end
--- 			local cfg = model_cfgs[model]
--- 			if not cfg then
--- 				vim.notify("Invalid model name: " .. model, vim.log.levels.ERROR)
--- 				return
--- 			end
--- 			keymap_set_chat(fetchers[model], bufnr)
--- 			vim.api.nvim_buf_set_lines(bufnr, 0, 1, false, {
--- 				string.format("model: %s", cfg.name),
--- 			})
--- 		end
--- 	end, {
--- 		nargs = 1,
--- 		range = true,
--- 		complete = function()
--- 			return { "switch" }
--- 		end,
--- 	})
--- end
+-- // INPUT PARSERS --
 
 ---@param raw any
 ---@return Mana2.ModelConfig|nil, string?
@@ -330,91 +320,93 @@ local function parse_opts_models(raw)
 end
 
 ---@param raw any
----@return table<Mana2.Endpoint, {url: string, env: string}>|nil, string?
+---@return Mana2.EndpointConfig|nil, string?
 local function parse_opts_endpoints(raw)
 	if type(raw) ~= "table" then
 		return nil, "endpoints must be a table"
 	end
 
 	local parsed = {}
-	for endpoint_name, config in pairs(raw) do
+	for endpoint, config in pairs(raw) do
 		if type(config.url) ~= "string" then
-			return nil, string.format("endpoint %s: url must be a string", endpoint_name)
+			return nil, string.format("endpoint %s: url must be a string", endpoint)
 		end
 		if type(config.env) ~= "string" then
-			return nil, string.format("endpoint %s: env must be a string", endpoint_name)
+			return nil, string.format("endpoint %s: env must be a string", endpoint)
 		end
 
-		parsed[endpoint_name] = {
+		local api_key = os.getenv(config.env)
+		if not api_key then
+			local tmp = "endpoint %s: API key not found in environment variable %s"
+			return nil, string.format(tmp, endpoint, config.env)
+		end
+
+		parsed[endpoint] = {
 			url = config.url,
-			env = config.env,
+			api_key = api_key,
 		}
 	end
 
 	return parsed
 end
 
+---@param raw any
+---@param model_cfgs Mana2.ModelConfig
+---@return Mana2.ModelConfig|nil, string?
+local function parse_opts_default_model(raw, model_cfgs)
+	if type(raw) ~= "string" then
+		return nil, "default_model must be a string"
+	end
+
+	if not model_cfgs[raw] then
+		local tmp = "default model '%s' not found in keys of models table"
+		return nil, string.format(tmp, raw)
+	end
+
+	return { [raw] = model_cfgs[raw] }
+end
+
 M.setup = function(opts)
-	local parsed_opts_models, models_err = parse_opts_models(opts.models)
-	if not parsed_opts_models then
-		vim.notify("Mana Error: " .. models_err, vim.log.levels.ERROR)
+	local model_cfgs, m_err = parse_opts_models(opts.models)
+	if not model_cfgs then
+		vim.notify("Mana.nvim error: " .. m_err, vim.log.levels.ERROR)
 		return
 	end
 
-	local parsed_opts_endpoints, endpoints_err = parse_opts_endpoints(opts.endpoints)
-	if not parsed_opts_endpoints then
-		vim.notify("Mana Error: " .. endpoints_err, vim.log.levels.ERROR)
+	local endpoint_cfgs, e_err = parse_opts_endpoints(opts.endpoints)
+	if not endpoint_cfgs then
+		vim.notify("Mana.nvim error: " .. e_err, vim.log.levels.ERROR)
 		return
 	end
 
-	---@type Mana2.EndpointConfig
-	local endpoint_cfgs = mk_endpoint_cfgs(parsed_opts_endpoints)
-	if vim.tbl_count(endpoint_cfgs) == 0 then
-		vim.notify("Mana Error: no API key found.", vim.log.levels.ERROR)
+	local default_model_cfg, dm_err = parse_opts_default_model(opts.default_model, model_cfgs)
+	if not default_model_cfg then
+		vim.notify("Mana.nvim error: " .. dm_err, vim.log.levels.ERROR)
 		return
 	end
 
-	---@type Mana2.ModelConfig
-	local model_cfgs = {} -- models without api keys wont be included
-	for model_name, model_config in pairs(parsed_opts_models) do
-		if endpoint_cfgs[model_config.endpoint] then
-			model_cfgs[model_name] = model_config
-		end
-	end
-
-	---@type Mana2.ModelConfig_
-	local default_model_cfg
-	if model_cfgs[opts.default_model] then
-		default_model_cfg = model_cfgs[opts.default_model]
-	else
-		for _, model_cfg in pairs(model_cfgs) do
-			default_model_cfg = model_cfg
-			break
-		end
-	end
-
-	vim.notify(vim.inspect(endpoint_cfgs))
 	vim.notify(vim.inspect(model_cfgs))
+	vim.notify(vim.inspect(endpoint_cfgs))
 	vim.notify(vim.inspect(default_model_cfg))
 
+	-- ---@type table<Mana2.Model, function>
 	-- local fetchers = {}
 	-- for model, model_cfg in pairs(model_cfgs) do
 	-- 	local endpoint_cfg = endpoint_cfgs[model_cfg.endpoint]
 	-- 	fetchers[model] = fetch(model_cfg, endpoint_cfg, 1)
 	-- end
 	--
-	local buffer_state = {
-		bufnr = buffer_get(),
-		winid = nil,
-	}
-
-	local prepend = string.format("model: %s\n\n<user>\n\n", default_model_cfg.name)
-	vim.api.nvim_buf_set_lines(buffer_state.bufnr, 0, -1, false, vim.split(prepend, "\n"))
-
-	keymap_set_ui(buffer_state.bufnr)
-	command_set_ui(buffer_state.bufnr, buffer_state.winid)
-
-	-- keymap_set_chat(buffer_state.bufnr, fetchers[default_model])
+	-- local buffer_state = {
+	-- 	bufnr = buffer_get(),
+	-- 	winid = nil,
+	-- }
+	--
+	-- local prepend = string.format("model: %s\n\n<user>\n\n", default_model_cfg.name)
+	-- vim.api.nvim_buf_set_lines(buffer_state.bufnr, 0, -1, false, vim.split(prepend, "\n"))
+	--
+	-- keymap_set_ui(buffer_state.bufnr)
+	-- command_set_ui(buffer_state.bufnr, buffer_state.winid)
+	-- keymap_set_chat(buffer_state.bufnr, fetchers[default_model_cfg.name])
 	-- command_set_chat(buffer_state.bufnr, model_cfgs, fetchers)
 end
 
